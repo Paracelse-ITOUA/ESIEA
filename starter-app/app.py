bd588dbc461d84383ee7e14e524e389c44a15570
Paracelse ITOUA <paracelse.itoua@hotmail.com>
2026-09-24 15:24:16 +0200

    feature/health séance 4 - etape 1

--- starter-app/app.py
+++ starter-app/app.py
@@ -29,7 +29,12 @@ def get_redis_client():
 
 @app.route("/health")
 def health():
-    return jsonify(status="ok"), 200
+    try:
+        client = get_redis_client()
+        client.ping()
+        return jsonify(status="ok"), 200
+    except redis.RedisError:
+        return jsonify(status="error"), 503
 
 
 @app.route("/status")
